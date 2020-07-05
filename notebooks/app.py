7f34c977bcd19b64f90fd5986705c3c5e7412b0f
mkozar101 <56281003+mkozar101@users.noreply.github.com>
2020-07-05 15:46:51 -0500

    mike intial push of app.py

---
+++ notebooks/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, jsonify, json, redirect
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import json
+import ast
+import os
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "flu vaccine machine learning project"
+
+
+@app.route("/query")
+def query():
+    engine = create_engine('postgresql://postgres:postgres@localhost/vaccine_db')
+    conn = engine.connect()
+    data = pd.read_sql("SELECT * FROM flu_vaccine_prediction", conn)
+    json_data = data.to_json(orient ='records')
+    return json_data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
