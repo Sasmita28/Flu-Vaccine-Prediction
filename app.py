31beca4ae49d576266fac9e29fabd065f7fc9924
Sasmita28 <27909312+Sasmita28@users.noreply.github.com>
2020-07-09 16:21:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,32 @@ import json
 import ast
 import os
 import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import MetaData
+from sqlalchemy import Table
+from sqlalchemy import Column
+from sqlalchemy import Integer, String, Float
+
+# from config1 import password
+
+
 
 app = Flask(__name__)
+
+
+# DATABASE_URL will contain the database connection string:
+
+engine = create_engine("postgres://doinivwtqywzqy:e1dd012f34fb6b9dd77449a82b9a52dbd6c0dd87b41921bf53ae80cd4f062350@ec2-34-206-31-217.compute-1.amazonaws.com:5432/d28e0pjlrkl4ui")
+
+
+# Create connection
+conn = engine.connect()
+# Begin transaction
+
+
 
 
 @app.route("/")
@@ -20,11 +44,18 @@ def index():
 
 @app.route("/query")
 def query():
-    engine = create_engine('postgresql://postgres:postgres@localhost/vaccine_db')
-    conn = engine.connect()
-    data = pd.read_sql("SELECT * FROM flu_vaccine_prediction", conn)
-    json_data = data.to_json(orient ='records')
+    
+    # engine = create_engine('postgresql://postgres:sasmita@localhost/vaccine_db')
+    # conn = engine.connect()
+    trans = conn.begin()
+    data = conn.execute('SELECT * FROM '
+                        '"flu_vaccine_prediction"')
+    json_data = jsonify(data)
     return json_data
+
+    trans.commit()
+    # Close connection
+    conn.close()
 
 
 if __name__ == "__main__":
