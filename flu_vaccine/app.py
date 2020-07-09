26be65c19e066301dbea1cad157b76bd01eb8919
sasmitapraveen <sasmitapraveen@gmail.com>
2020-07-09 13:01:18 -0500

    files

--- flu_vaccine/app.py
+++ flu_vaccine/app.py
@@ -8,38 +8,92 @@ import ast
 import os
 import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import MetaData
+from sqlalchemy import Table
+from sqlalchemy import Column
+from sqlalchemy import Integer, String, Float
 
-app = Flask(__name__)
+# app = Flask(__name__)
+# load our prediction dataset to pandas
+
+df = pd.read_csv('sample_prediction_set.csv')
+
+# DATABASE_URL will contain the database connection string:
+
+engine = create_engine("postgres://doinivwtqywzqy:e1dd012f34fb6b9dd77449a82b9a52dbd6c0dd87b41921bf53ae80cd4f062350@ec2-34-206-31-217.compute-1.amazonaws.com:5432/d28e0pjlrkl4ui")
+
+# Create a metadata instance
+metadata = MetaData(engine)
+# Declare a table
+table = Table('flu_vaccine_prediction',metadata,
+Column('respondent_id',Integer),
+Column('id',Integer, primary_key= True),                        
+Column('h1n1_vaccine', Integer),                   
+Column('seasonal_vaccine', Integer),
+Column('h1n1_concern' ,  Float),               
+Column('h1n1_knowledge' , Float),               
+Column('behavioral_antiviral_meds', Float),     
+Column('behavioral_avoidance', Float),           
+Column('behavioral_face_mask' ,Float),         
+Column('behavioral_wash_hands', Float),        
+Column('behavioral_large_gatherings', Float),    
+Column('behavioral_outside_home',  Float),      
+Column('behavioral_touch_face', Float),         
+Column('doctor_recc_h1n1',  Float),             
+Column('doctor_recc_seasonal',Float),           
+Column('chronic_med_condition',  Float),        
+Column('child_under_6_months' ,Float),  
+Column('health_worker',   Float),  
+Column('opinion_h1n1_vacc_effective',Float),   
+Column('opinion_h1n1_risk' ,  Float),  
+Column('opinion_h1n1_sick_from_vacc',Float), 
+Column('opinion_seas_vacc_effective' , Float),  
+Column('opinion_seas_risk' ,  Float),     
+Column('opinion_seas_sick_from_vacc',  Float), 
+Column('age_group' ,  String),    
+Column('education', String),  
+Column('race' , String),  
+Column('sex' ,  String),   
+Column('income_poverty', String), 
+Column('marital_status', String),  
+Column('rent_or_own',  String),  
+Column('employment_status' ,  String), 
+Column('hhs_geo_region' ,String),  
+Column('census_msa'  ,    String),   
+Column('household_adults',   Float),   
+Column('household_children',   Float),    
+Column('Prediction' ,     Integer) )
+# Create all tables
+metadata.create_all()
+
+df.to_sql(name='flu_vaccine_prediction',con=engine, if_exists='replace', index=False)
+
+print(table.name)
+# print(table.columns.keys())
+
+# result = engine.execute('SELECT * FROM '
+#                         '"flu_vaccine_prediction"')
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+#     # return "flu vaccine machine learning project"
 
 
-# # DATABASE_URL will contain the database connection string:
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# # Connects to the database using the app config
-# vaccine_db = SQLAlchemy(app)
-DATABASE_URL = os.environ['DATABASE_URL']
-engine = create_engine(DATABASE_URL/'vaccine_db')
-conn = engine.connect()
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-# from .models import Flu_Vaccine
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-    # return "flu vaccine machine learning project"
-
-
-
-@app.route("/query")
-def query():
+# @app.route("/query")
+# def query():
     
-    # engine = create_engine('postgresql://postgres:sasmita@localhost/vaccine_db')
-    # conn = engine.connect()
-    data = pd.read_sql("SELECT * FROM flu_vaccine_prediction", conn)
-    json_data = data.to_json(orient ='records')
-    return json_data
+#     # engine = create_engine('postgresql://postgres:sasmita@localhost/vaccine_db')
+#     # conn = engine.connect()
+#     data = engine.execute('SELECT * FROM '
+#                         '"flu_vaccine_prediction"')
+#     json_data = jsonify(data)
+#     return json_data
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
