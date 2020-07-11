b82f213d25b708a6062c240862d93286dd01fa51
Sasmita28 <27909312+Sasmita28@users.noreply.github.com>
2020-07-11 13:26:34 -0500

    Update app.js

--- flu_vaccine/static/js/app.js
+++ flu_vaccine/static/js/app.js
@@ -1,34 +1,33 @@
-
 d3.json('/query').then(data => {
-
+​
   
     //selecting the 'Id_info" element from html to append 'ul'
     var ul = d3.select(".Id_info").append("ul");
-
+​
     // selecting the button1(click to know about me button)
     var button1 = d3.select(".button1")
-
-
+​
+​
     // Handling click for button1
     button1.on("click", function() {  
-
+​
         // emptying the page for every click
         ul.html("");
-
-
+​
+​
         // finding a random number between 0 and 29 for every click
         number = Math.floor((Math.random() * 29) + 1);
-
+​
         // finding random number 0 or 1 for random pick of our paragraph
         random_pick = Math.round(Math.random());
-
+​
         console.log(`random number for paragraph: ${random_pick}`);
         console.log(`number for id: ${number}`);
-
+​
         // extracting data when number = id 
         info = data.filter( element => element.id == number)
         // console.log(info);
-
+​
         // extracting data from info to our variables
         res_id = info.map(row=> row.respondent_id);
         sex = info.map(row=> row.sex);
@@ -45,7 +44,7 @@ d3.json('/query').then(data => {
   
         // initializing the binary_cols dictionary to convert 0 and 1 to 'NO' and 'YES' respectively
         binary_cols = {}
-
+​
         // assigning keys and values to binary_cols
         binary_cols= {'wash_hands':wash_hands,'touch_face':touch_face,'wear_mask':wear_mask,'large_gathering':large_gathering,'truth':truth,'prediction':prediction}
         
@@ -57,11 +56,11 @@ d3.json('/query').then(data => {
             else {
                 binary_cols[key] = 'NO'
             }
-
+​
         })
         // console.log(binary_cols);
-
-
+​
+​
         // creating details1 and details2 dictionaries for our paragraphs
         details1 = {}
         details1['My Id'] = res_id[0]
@@ -71,7 +70,7 @@ d3.json('/query').then(data => {
         details1['My Income'] = income[0]
         details1['I wash my hands often'] =binary_cols['wash_hands']
         details1['I touch my face often'] = binary_cols['touch_face']
-
+​
         details2 = {}
         details2['My Id'] = res_id[0]
         details2['Sex'] = sex[0]
@@ -80,12 +79,12 @@ d3.json('/query').then(data => {
         details2['My education'] = education[0]
         details2['Do I regularly wear a face mask'] = binary_cols['wear_mask']
         details2['Do I like large gatherings'] = binary_cols['large_gathering']
-
-
-
+​
+​
+​
         
     //    console.log(details1)
-
+​
         // Creating the list elements in html using details1 dictionary
         if (random_pick == 0) {
             Object.entries(details1).forEach(([key,value]) =>{
@@ -104,36 +103,36 @@ d3.json('/query').then(data => {
       
             
     });
-
+​
     // handling the 'YES' and 'NO' buttons
     var button_grp = d3.selectAll(".btn-group")
     // console.log(button_grp);
     button_grp.on("click", function() {  
-
+​
         var truth = binary_cols['truth']
         var prediction = binary_cols['prediction']
-
+​
         var guess = d3.event.target.value
-
+​
         if (guess == truth && truth != prediction) {
-
+​
             var test = 'You beat the Model'
-
+​
         }
         else if (guess != truth && truth == prediction) {
-
+​
             var test = 'The Model beat you'
-
+​
         }
         else if (guess != truth && truth != prediction) {
-
+​
             var test = 'You both are wrong'
-
+​
         }
         else {
             var test = 'You both are correct'
         }
-
+​
             if (guess == 'YES') {
                 your_prediction = 'would'
             }
@@ -146,56 +145,57 @@ d3.json('/query').then(data => {
             else{
                model_prediction = 'would not'
             }
-
+​
             if (truth == 'YES') {
                 truth_response= 'did'
             }
             else{
                 truth_response = 'did not'
             }
-
-
+​
+​
             var results  = `${test}. You guessed that they ${your_prediction} get a vaccine. The model predicted that they ${model_prediction} get a vaccine. In truth, they ${truth_response} get a vaccine.`
             
-
-
+​
+​
             d3.select("#final_answer").html(results);
             
             
-
-
+​
+​
     });
-
-
+​
+​
 });
-
+​
 // Get the modal
 var modal = document.getElementById("myModal");
-
+​
 // Get the button that opens the modal
 var btn = document.getElementById("yesNobuttons");
-
+​
 //Get the card
 var cardElement = document.getElementById("cardbody");
-
+​
 //Get the button that clears the card
 var btn2 = document.getElementById("tryAgain");
-
-
+​
+​
 // When the user clicks on the yes.no buttons, open the results modal popup
 yesNobuttons.onclick = function() {
 modal.style.display = "block";
 }
-
+​
 //When the "Try Again" button is clicked, the popup disappears and the survey card clears
 tryAgain.onclick = function() {
-modal.style.display = "none";
-cardbody.style.display = "none";
-
-
+window.location.reload();
+​
+​
+​
+​
 }
-
-//"click to learn about me" button displays next survey data after page load, after starting over on page.
+​
+//"click to learn about me" button displays next survey; data after page load, after starting over on page.
 button1.onclick = function() {
 cardbody.style.display = "block";
 }
